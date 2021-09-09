2a8ba4918619c136b9cf973f1070974c153c2de8
ju1pp1 <61796625+ju1pp1@users.noreply.github.com>
2021-09-09 12:54:14 +0300

    Add files via upload

---
+++ Osa2/2.9/src/components/Note.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+    const Note = ({ person }) => {
+        return (
+            <ul>
+                {person.name} {person.number}
+                {person.content} 
+                
+            </ul>
+     )
+    }
+
+export default Note
