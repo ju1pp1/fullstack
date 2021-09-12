11406e269e792ced5da1ee97f7ece9869478fc55
ju1pp1 <61796625+ju1pp1@users.noreply.github.com>
2021-09-12 17:35:26 +0300

    Add files via upload

---
+++ Osa2/2.8/src/src/index.js
@@ -0,0 +1,37 @@
+import ReactDOM from 'react-dom'
+import App from './App.js'
+
+const persons = [
+    {
+        id: 1,
+        content: 'HTML is easy',
+        date: '2019-05-30T17:30:31.098Z',
+        important: true,
+        name: [],
+        number: []
+    },
+    {
+        id: 2,
+        content: 'Browser can execute only JavaScript',
+        date: '2019-05-30T18:39:34.091Z',
+        important: false,
+        name: [],
+        number: []
+    },
+    {
+        id: 3,
+        content: 'GET and POST are the most important methods of HTTP protocol',
+        date: '2019-05-30T19:20:14.298Z',
+        important: true,
+        name: [],
+        number: []
+    }
+]
+
+const result = persons.map(person => person.content)
+console.log(result)
+
+ReactDOM.render(
+    <App persons={persons} />,
+    document.getElementById('root')
+)
